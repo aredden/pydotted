aa1c860c7302d64b3c5376032732127cae42a8bd
aredden <alexander.h.redden@gmail.com>
2021-12-18 20:31:54 -0500

    Adding package setup configuration & license

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "pydotted"
 LONG_DESCRIPTION = "pydotted is a very simple low code python module that allows for dot notation access / updating of a dictionary instance, including nested dictionaries."
 
@@ -13,7 +13,13 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    python_requires=">=3.6",
     install_requires=[],
     keywords=["python", "dict", "dot", "dotdict", "dot notation", "pydotted"],
-    classifiers=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
 )
