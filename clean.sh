62ad1d3eaccc65edc94b3cf4a0673ad089a29c6a
aredden <alexander.h.redden@gmail.com>
2022-01-08 13:18:53 -0500

    Adding helper scripts

---
+++ clean.sh
@@ -0,0 +1,7 @@
+rm -rf ./*/__pycache__/ &&
+    rm -rf ./htmlcov/* &&
+    rm -rf ./build/* &&
+    rm -rf ./dist/* &&
+    rm -rf ./*.egg-info/ &&
+    rm -rf ./.pytest_cache &&
+    rm -rf __pycache__/
